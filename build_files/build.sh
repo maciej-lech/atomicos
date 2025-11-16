d0a049905a077ac1af17e9e42e49204c189e778a
Maciej Lech <maciej.lech@mlit.pro>
2025-11-16 15:28:57 +0100

    Install Cosmic DE

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,18 +2,21 @@
 
 set -ouex pipefail
 
-# Install from Fedora
-
-dnf5 install -y \
-  helix \
-  tmux
-
-# Install from COPR
-
+# Enable COPR repos
 dnf5 -y copr enable fcsm/tmuxinator
-dnf5 -y install tmuxinator
+dnf5 -y copr enable ryanabx/cosmic-epoch
+
+# Install packages
+dnf5 install -y \
+  @cosmic-desktop \
+  @cosmic-desktop-apps \
+  helix \
+  tmux \
+  tmuxinator
+
+# Disable COPR repos
 dnf5 -y copr disable fcsm/tmuxinator
+dnf5 -y copr disable ryanabx/cosmic-epoch
 
 # Clean up
-
 dnf5 clean all
