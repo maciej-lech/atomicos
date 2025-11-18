71ea3558b9b43ff5cf123a589a63c4cb6e3e3041
Maciej Lech <maciej.lech@mlit.pro>
2025-11-18 15:46:18 +0100

    Add ghostty

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,18 +4,21 @@ set -ouex pipefail
 
 # Enable COPR repos
 dnf5 -y copr enable fcsm/tmuxinator
+dnf5 -y copr enable scottames/ghostty
 dnf5 -y copr enable ryanabx/cosmic-epoch
 
 # Install packages
 dnf5 install -y \
   @cosmic-desktop \
   @cosmic-desktop-apps \
+  ghostty \
   helix \
   tmux \
   tmuxinator
 
 # Disable COPR repos
 dnf5 -y copr disable fcsm/tmuxinator
+dnf5 -y copr disable scottames/ghostty
 dnf5 -y copr disable ryanabx/cosmic-epoch
 
 # Clean up
