7423050ad927812f5244c6cb61ab0307b177b228
Maciej Lech <maciej.lech@mlit.pro>
2025-11-19 15:21:53 +0100

    Remove cosmic

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,12 +5,9 @@ set -ouex pipefail
 # Enable COPR repos
 dnf5 -y copr enable fcsm/tmuxinator
 dnf5 -y copr enable scottames/ghostty
-dnf5 -y copr enable ryanabx/cosmic-epoch
 
 # Install packages
 dnf5 install -y \
-  @cosmic-desktop \
-  @cosmic-desktop-apps \
   ghostty \
   helix \
   tmux \
@@ -19,7 +16,6 @@ dnf5 install -y \
 # Disable COPR repos
 dnf5 -y copr disable fcsm/tmuxinator
 dnf5 -y copr disable scottames/ghostty
-dnf5 -y copr disable ryanabx/cosmic-epoch
 
 # Clean up
 dnf5 clean all
