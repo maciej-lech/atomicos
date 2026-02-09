84c534c914689f67687b6d89043e21e2b1c6bffa
Maciej Lech <maciej.lech@mlit.pro>
2026-02-09 17:32:17 +0100

    Add podman-bootc

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,14 +5,17 @@ set -ouex pipefail
 # Enable COPR repos
 dnf5 -y copr enable fcsm/tmuxinator
 dnf5 -y copr enable scottames/ghostty
+dnf5 -y copr enable gmaglione/podman-bootc
 
 # Install packages
 dnf5 install -y \
   ghostty \
   helix \
+  podman-bootc \
   tmux \
   tmuxinator
 
 # Disable COPR repos
-dnf5 -y copr disable fcsm/tmuxinator
+dnf5 -y copr disable gmaglione/podman-bootc
 dnf5 -y copr disable scottames/ghostty
+dnf5 -y copr disable fcsm/tmuxinator
