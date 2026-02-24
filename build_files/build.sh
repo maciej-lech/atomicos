2cfa9ef62d15b689bc37acbb1fe455412bcf109c
Maciej Lech <maciej.lech@mlit.pro>
2026-02-24 08:29:36 +0100

    Remove podman-bootc

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,20 +5,17 @@ set -ouex pipefail
 # Enable COPR repos
 dnf5 -y copr enable fcsm/tmuxinator
 dnf5 -y copr enable scottames/ghostty
-dnf5 -y copr enable gmaglione/podman-bootc
 
 # Install packages
 dnf5 install -y \
   fsverity-utils \
   ghostty \
   helix \
-  podman-bootc \
   qt6ct \
   socat \
   tmux \
   tmuxinator
 
 # Disable COPR repos
-dnf5 -y copr disable gmaglione/podman-bootc
 dnf5 -y copr disable scottames/ghostty
 dnf5 -y copr disable fcsm/tmuxinator
