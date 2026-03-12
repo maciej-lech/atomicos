5fa1a02c2bf994c09c52b9143c42326e04ddc10c
Maciej Lech <maciej.lech@mlit.pro>
2026-03-12 18:37:59 +0100

    Switch from GDM to greetd with smooth plymouth transition

--- build/16-graphical.sh
+++ build/16-graphical.sh
@@ -18,13 +18,6 @@ dnf5 -y install \
   --enablerepo="coprdep:copr.fedorainfracloud.org:avengemedia:danklinux" \
   cliphist dms dms-greeter niri waypipe
 
-cat > /usr/lib/sysusers.d/dms-greeter.conf <<'SYSUSERS'
-u greeter 962 "System Greeter" /var/lib/greeter /bin/bash
-SYSUSERS
+systemctl disable gdm.service
+systemctl enable greetd.service
 
-cat > /usr/lib/tmpfiles.d/dms-greeter.conf <<'TMPFILES'
-d /var/lib/greetd/.config 0755 greetd greetd - -
-d /var/lib/greetd/.config/systemd 0755 greetd greetd - -
-d /var/lib/greetd/.config/systemd/user 0755 greetd greetd - -
-L /var/lib/greetd/.config/systemd/user/xdg-desktop-portal.service - - - - /dev/null
-TMPFILES
